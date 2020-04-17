08d969de56d9fc50a74a540eb2ac23f645abe0fe
chip-dale <62562443+chip-dale@users.noreply.github.com>
2020-04-17 04:21:02 +0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -20,6 +20,7 @@ $(function(){
     });
  });/* фиксированное меню вверху страницы при прокрутке */
 
+
 		/* слайдер на главной странице */
 function formatText(index, panel) {
 	return index + "";
@@ -30,7 +31,7 @@ $(function () {
 		autoPlay: true,					// This turns off the entire FUNCTIONALY, not just if it starts running or not.
 		delay: 3600,					// How long between slide transitions in AutoPlay mode
 		startStopped: false,			// If autoPlay is on, this can force it to start stopped
-		animationTime: 1200,				// How long the slide transition takes
+		animationTime: 1200,			// How long the slide transition takes
 		hashTags: true,					// Should links change the hashtag in the URL?
 		buildNavigation: false,			// If true, builds and list of anchor links to link to each slide
 		pauseOnHover: true,				// If true, and autoPlay is enabled, the show will pause on hover
@@ -42,3 +43,26 @@ $(function () {
 		$('.anythingSlider').anythingSlider();
 	});
 });/* слайдер на главной странице */
+
+		// Gallery
+lightbox.option({
+  albumLabel: 'Image %1 of %2',
+  alwaysShowNavOnTouchDevices: false,
+  fadeDuration: 600,
+  fitImagesInViewport: true,
+  imageFadeDuration: 600,
+  maxWidth: 800,
+  maxHeight: 600,
+  positionFromTop: 50,
+  resizeDuration: 700,
+  showImageNumberLabel: true,
+  wrapAround: false, // If true, when a user reaches the last image in a set, the right navigation arrow will appear and they will be to continue moving forward which will take them back to the first image in the set.
+  /*
+  Sanitize Title
+  If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
+  This will free you to add html tags, such as links, in the caption.
+  If the caption data is user submitted or from some other untrusted source, then set this to true
+  to prevent xss and other injection attacks.
+   */
+  sanitizeTitle: false
+})
