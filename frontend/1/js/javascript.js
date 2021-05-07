7e40df183da6e16ae4b01b1e20a1fb1bf93a63a6
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-05-07 15:54:29 +0400

    Add files via upload

---
+++ frontend/1/js/javascript.js
@@ -0,0 +1,12 @@
+var typed5 = new Typed('#typed', {
+	strings: ['Hello World!^1600\n<br>My name is Fidan Ismailova^1600\n<br>I\'m Web-Developer^1600\n<br>Live in Azerbaijan'],
+	typeSpeed: 50,
+	backSpeed: 0,
+	backDelay: 200,
+	startDelay: 1500,
+	showCursor: true,
+	cursorChar: '_',
+	shuffle: false,
+	smartBackspace: false,
+	loop: false
+});
